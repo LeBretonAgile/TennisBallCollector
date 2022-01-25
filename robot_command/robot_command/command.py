28893b09ec12e47b5938a88a9568dd7cf2506cac
Jadoulth <jadoul.thibault@outlook.fr>
2022-01-25 09:48:30 +0100

    marche pas

--- robot_command/robot_command/command.py
+++ robot_command/robot_command/command.py
@@ -1,21 +1,28 @@
 import sys
-import rclpy
 import signal
 import time
 import numpy as np
+import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-from sensor_msgs.msg import LaserScan
-from robot_command.msg import Balls_pos
+from robot_command.msg import Tuple
 
 class Commande(Node):
 	def __init__(self):
 		super().__init__("command")
 		self.cmd_vel_publisher = self.create_publisher(Twist, "/cmd_vel", 10)
-		self.pos_subscriber = self.create_subscription(Balls_pos, "/pos", self.callback, 10)
+		self.pos_subscriber = self.create_subscription(Tuple, "/pos", self.callback, 10)
+		
+
+		self.ball = np.array([0],[0])
 
 	def callback(self, msg):
 		self.get_logger().info(f"Received message : {msg}")
+		self.ball[0] = msg.x
+		self.ball[1] = msg.y
+
+	def commande(self):
+		pass
 
 	def shutdown(self, signum, frame):
 		twist = Twist()
@@ -27,5 +34,9 @@ class Commande(Node):
 def main(args=None):
 	rclpy.init(args=args)
 	node = Commande()
-	signal.signal(signal.SIGINT, node.shutdown)
-	rclpy.spin(node)
+	rate = rclpy.create_rate(30)
+	while not rclpy.ok():
+		node.commande()
+		signal.signal(signal.SIGINT, node.shutdown)
+		rclpy.spin(node)
+		rate.sleep()
--- robot_command/setup.py
+++ robot_command/setup.py
@@ -20,7 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-        'camera_top = robot_command.camera_top:main'
+        'camera_top = robot_command.camera_top:main',
+        'command = robot_command.command:main'
         ],
     },
 )
