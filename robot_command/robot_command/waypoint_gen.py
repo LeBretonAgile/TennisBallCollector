e7359d36a1c416984a1823214f58dfb276579e52
Bernardo Hummes <hummescmpa@gmail.com>
2022-02-21 10:50:16 +0100

    initial version of the waypoint generation

--- robot_command/robot_command/waypoint_gen.py
+++ robot_command/robot_command/waypoint_gen.py
@@ -24,21 +24,37 @@ def numpy_to_multiarray(arr, arr_type):
 
     return multiarray
 
+
+def multiarray_to_numpy(multiarr, multiarr_type, arr_type):
+    dims = map(lambda x: x.size, multiarr.layout.dim)
+    return np.array(multiarr.data, dtype=multiarr_type).reshape(dims).astype(arr_type)
+
+
 class WaypointNode(Node):
     def __init__(self):
         super().__init__("waypoint_gen")
         self.waypoint_publisher = self.create_publisher(Pose, "/waypoint", 30)
-        self.ball_subscriber = self.create_subscription(Float64MultiArray, "/ball_list", self.ball_callback, 30)
+        self.ball_subscriber = self.create_subscription(Float64MultiArray, "/balls", self.ball_callback, 30)
         self.pose = Pose()
         self.waypoint = Float64MultiArray()
 
     def ball_callback(self, msg):
-        balls_shape = msg.layout
-        balls = msg.data
-#        print("layout:", balls_shape)
-#        print("data:", balls)
+        balls = multiarray_to_numpy(msg, np.float64, np.float64)
+        print("balls shape:", balls.shape)
+        print("balls data:", balls)
 
-        self.get_logger().info("Received message with layout: " + str(balls_shape) + "\and data:\n" + str(balls))
+        missing_balls = False
+        for ball in balls:
+            if ball[0] > 0 or ball[1] > 0:
+                missing_balls = True
+                ball_pos = ball
+
+        self.get_logger().info("Received message with layout: " + str(balls.shape) + "\and data:\n" + str(balls))
+
+        self.waypoint.data.position.x = ball_pos[0]
+        self.waypoint.data.position.y = ball_pos[1]
+
+        self.waypoint_publisher.publish(self.waypoint)
 
 
 def main():
